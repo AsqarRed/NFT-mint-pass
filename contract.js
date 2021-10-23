0f47c9b60617edfefe91f3d04b7da463407d3251
theshadowagent <shadowagent1st@gmail.com>
2021-10-23 20:00:31 +0300

    Fixed abi

--- contract.js
+++ contract.js
@@ -2,6 +2,7 @@ import { ALLOWED_NETWORKS, AMEEGOS_PASS_CONTRACT } from './contracts/ameegos.js'
 import { getCurrentNetwork, web3 } from './wallet.js';
 
 const currentNetwork = await getCurrentNetwork();
+console.log(currentNetwork)
 if (!ALLOWED_NETWORKS.includes(currentNetwork)) {
     alert("You're on the wrong network. Please try switching to mainnet or Rinkeby, and refresh the page")
 }
@@ -14,7 +15,7 @@ if (window?.passContract?.websiteURL?.includes(window.location.hostname)) {
     abi = window.passContract.abi;
 } else if (window.location.hostname.includes('ameegos.io')) {
     address = AMEEGOS_PASS_CONTRACT[currentNetwork].address;
-    abi = AMEEGOS_PASS_CONTRACT[currentNetwork].abi;
+    abi = AMEEGOS_PASS_CONTRACT.abi;
 }
 
 export const contract = new web3.eth.Contract(abi, address);
