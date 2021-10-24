5666437500909c726f5a654d94f64bc258b90a2d
theshadowagent <shadowagent1st@gmail.com>
2021-10-24 19:15:21 +0300

    Fix Sentry

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import { insertClaimLink } from "./claim.js";
+
+insertClaimLink();
+
 Sentry.init({
     dsn: "https://bc944dd29060483e8e84a40fe22fffad@o1050370.ingest.sentry.io/6031616",
     integrations: [],
@@ -6,7 +10,3 @@ Sentry.init({
     // for finer control
     tracesSampleRate: 1.0,
 });
-
-import { insertClaimLink } from "./claim.js";
-
-insertClaimLink();
