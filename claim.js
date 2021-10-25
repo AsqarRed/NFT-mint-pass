64ec14a8daa2a5e60cec332fce153262adfb2bed
theshadowagent <shadowagent1st@gmail.com>
2021-10-25 12:55:46 +0300

    Added loading indicator

--- claim.js
+++ claim.js
@@ -1,7 +1,9 @@
 import { contract } from "./contract.js";
 import { getWalletAddress } from "./wallet.js";
 
-const claimMintPass = async () => {
+const claimMintPass = async (button) => {
+    const previousBtnText = button.textContent;
+    button.textContent = "Loading...";
     const wallet = await getWalletAddress();
     let quantity = Number(document.querySelector("#select-quantity")?.value ?? 1);
     quantity = quantity === 0 ? 1 : quantity;
@@ -10,6 +12,7 @@ const claimMintPass = async () => {
         from: wallet
     }
     const estimatedGas = await tx.estimateGas(txData).catch((e) => {
+        button.textContent = previousBtnText;
         const message = e.message.split("{")[0].trim();
         alert(`Error ${message}. Please try refreshing page, check your MetaMask connection or contact us to resolve`);
         console.log(e);
@@ -20,14 +23,16 @@ const claimMintPass = async () => {
     console.log(estimatedGas)
     tx.send({...txData, gasLimit: estimatedGas + 5000})
       .catch((e) => {
-            if (e.code !== 4001) {
-                const message = e.message.split("{")[0].trim();
-                alert(`Error ${message}. Please try refreshing page, checking your MetaMask connection or contact us to resolve`);
-                console.log(e);
-            }
+          button.textContent = previousBtnText;
+          if (e.code !== 4001) {
+            const message = e.message.split("{")[0].trim();
+            alert(`Error ${message}. Please try refreshing page, checking your MetaMask connection or contact us to resolve`);
+            console.log(e);
+          }
       })
       .then((r) => {
-            console.log(r);
+          button.textContent = previousBtnText;
+          console.log(r);
       })
 
 };
@@ -36,7 +41,7 @@ export const insertClaimLink = () => {
     const claimButton = document.querySelector("#claim-mint-pass");
     if (claimButton) {
         claimButton.onclick = () => {
-            claimMintPass();
+            claimMintPass(claimButton);
         }
     }
 }
