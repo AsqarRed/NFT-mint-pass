2ce1d636c13697e1007dd34eb4d431dd79efbd8b
theshadowagent <shadowagent1st@gmail.com>
2021-10-25 03:40:46 +0300

    Remove debug logs

--- wallet.js
+++ wallet.js
@@ -1,46 +1,26 @@
 export const web3 = new Web3(ethereum);
 
 const isMetamaskConnected = () => {
-    const isConnected = ethereum.selectedAddress !== null;
-    if (isDebugMode()) {
-        alert(`CHECKED IS_CONNECTED: ${isConnected}`)
-    }
-    return isConnected;
+    return ethereum.selectedAddress !== null;
 }
 
 export const getWalletAddress = async () => {
-    const currentAddress = async () => (
-        ethereum.selectedAddress ?? await ethereum.request({ method: 'eth_requestAccounts' })[0]
-    );
-    if (!await currentAddress()) {
-        await connectMetamask();
-    }
-    return await currentAddress();
+    return ethereum.selectedAddress ?? await ethereum.request({ method: 'eth_requestAccounts' })[0];
 }
 
 export const getCurrentNetwork = async () => {
     return Number(await ethereum.request({ method: 'net_version' }));
 }
 
-const isDebugMode = () => {
-    return window.location.href.includes("mint-pass-debug")
-}
-
 export const updateMetamaskStatus = () => {
     const connected = isMetamaskConnected()
-    if (isDebugMode()) {
-        alert(`METAMASK STATUS: CONNECTED ${connected}`)
-    }
     if (connected) {
-        const button = document.querySelector('#connect');
-        button.textContent = "MetaMask connected";
+        const button = document.querySelector(window.buttonID ?? '#connect');
+        button.textContent = "Metamask connected";
     }
 }
 
 export const connectMetamask = async () => {
-    if (isDebugMode()) {
-        alert(`CALLED CONNECT METAMASK`)
-    }
     await ethereum.request({ method: 'eth_requestAccounts' });
     updateMetamaskStatus();
 }
