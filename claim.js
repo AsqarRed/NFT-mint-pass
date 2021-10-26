0e185cd46d399d5f0bb94ee31b23b2cd190f5c6d
theshadowagent <shadowagent1st@gmail.com>
2021-10-26 19:03:17 +0300

    Added redeem

--- claim.js
+++ claim.js
@@ -1,4 +1,4 @@
-import { contract } from "./contract.js";
+import { passContract, NFTContract } from "./contract.js";
 import { getWalletAddress } from "./wallet.js";
 
 const claimMintPass = async (button) => {
@@ -7,7 +7,7 @@ const claimMintPass = async (button) => {
     const wallet = await getWalletAddress();
     let quantity = Number(document.querySelector("#select-quantity")?.value ?? 1);
     quantity = quantity === 0 ? 1 : quantity;
-    const tx = contract.methods.claim(quantity);
+    const tx = passContract.methods.claim(quantity);
     const txData = {
         from: wallet
     }
@@ -37,11 +37,54 @@ const claimMintPass = async (button) => {
 
 };
 
+const redeemMintPass = async (button) => {
+    const previousBtnText = button.textContent;
+    button.textContent = "Loading...";
+    const wallet = await getWalletAddress();
+    let quantity = Number(document.querySelector("#select-quantity")?.value ?? 1);
+    quantity = quantity === 0 ? 1 : quantity;
+    const tx = passContract.methods.redeem(quantity);
+    const price = await NFTContract.methods.cost().call();
+    const txData = {
+        from: wallet,
+        value: price * quantity
+    }
+    const estimatedGas = await tx.estimateGas(txData).catch((e) => {
+        button.textContent = previousBtnText;
+        const message = e.message.split("{")[0].trim();
+        alert(`Error ${message}. Please try refreshing page, check your MetaMask connection or contact us to resolve`);
+        console.log(e);
+    });
+    if (estimatedGas === undefined) {
+        return
+    }
+    console.log(estimatedGas)
+    tx.send({...txData, gasLimit: estimatedGas + 5000})
+        .catch((e) => {
+            button.textContent = previousBtnText;
+            if (e.code !== 4001) {
+                const message = e.message.split("{")[0].trim();
+                alert(`Error ${message}. Please try refreshing page, checking your MetaMask connection or contact us to resolve`);
+                console.log(e);
+            }
+        })
+        .then((r) => {
+            button.textContent = previousBtnText;
+            console.log(r);
+        })
+};
+
 export const insertClaimLink = () => {
     const claimButton = document.querySelector("#claim-mint-pass");
     if (claimButton) {
-        claimButton.onclick = () => {
-            claimMintPass(claimButton);
+        claimButton.onclick = async () => {
+            await claimMintPass(claimButton);
+        }
+    }
+    const redeemButton = document.querySelector("#redeem-mint-pass");
+    if (redeemButton) {
+        redeemButton.onclick = async () => {
+            await redeemMintPass(redeemButton);
         }
     }
 }
--- contract.js
+++ contract.js
@@ -1,22 +1,21 @@
-import { ALLOWED_NETWORKS, AMEEGOS_PASS_CONTRACT } from './contracts/ameegos.js';
+import {
+    ALLOWED_NETWORKS,
+    AMEEGOS_NFT_CONTRACT,
+    AMEEGOS_PASS_CONTRACT
+} from './contracts/ameegos.js';
 import { getCurrentNetwork, web3 } from './wallet.js';
 
-const currentNetwork = await getCurrentNetwork();
-console.log(currentNetwork)
-if (!ALLOWED_NETWORKS.includes(currentNetwork)) {
-    alert("You're on the wrong network. Please try switching to mainnet or Rinkeby, and refresh the page")
+const initContract = async (contract) => {
+    const currentNetwork = await getCurrentNetwork();
+    if (!ALLOWED_NETWORKS.includes(currentNetwork)) {
+        alert("You're on the wrong network. Please try switching to mainnet or Rinkeby, and refresh the page")
+    }
+    const address = contract.address[currentNetwork];
+    const abi = contract.abi;
+    return new web3.eth.Contract(abi, address);
 }
 
-export let address;
-export let abi;
-
-if (window?.passContract?.websiteURL?.includes(window.location.hostname)) {
-    address = window.passContract.address;
-    abi = window.passContract.abi;
-} else if (window.location.hostname.includes('ameegos.io')) {
-    address = AMEEGOS_PASS_CONTRACT[currentNetwork].address;
-    abi = AMEEGOS_PASS_CONTRACT.abi;
-}
-
-export const contract = new web3.eth.Contract(abi, address);
-window.contract = contract;
+export const NFTContract = await initContract(AMEEGOS_NFT_CONTRACT);
+export const passContract = await initContract(AMEEGOS_PASS_CONTRACT);
+window.passContract = passContract;
+window.NFTContract = NFTContract;
